8bb3e018dfff743c9fa2f93c7951ddb95c22ee19
apoc64 <sschwedt@gmail.com>
2019-11-17 11:17:14 -0700

    Initial Commit

---
+++ NEOTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NEOTracker
+//
+//  Created by Steven Schwedt on 11/17/19.
+//  Copyright © 2019 Steven Schwedt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
